5b4392432d63f6c50265384647997a04108d306a
Pranshu-007 <84780774+Pranshu-007@users.noreply.github.com>
2025-02-19 14:51:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
-
+	const fname = document.getElementById("fname");
+	const firstname = fname.value;
+	const lname = document.getElementById("lname");
+	const lastname = lname.value;
+	alert(`${firstname}${lastname}`)
 }
